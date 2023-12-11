4d4dd491b767c5ecb0b450e0f8ac2667adf7622c
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2023-12-11 23:52:43 +0100

    encoder

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Encoder\Encoder;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Serializer\Serializer;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -22,27 +23,30 @@ class UserController extends AbstractController
     #[Route('/api/users/read', name: 'app_users', methods: ['GET'], format: 'json')]
     public function readAll(UserRepository $userRepository): JsonResponse
     {
-        dd(Encoder::encode(json_encode(Serializer::serializeAll($userRepository->findAll())), User::class));
-        return $this->json(Serializer::serializeAll($userRepository->findAll()));
+        try {
+            return $this->json(Serializer::serializeAll($userRepository->findAll()));
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()]);
+        }
     }
 
     #[Route('/api/users/read/{id}', name: 'app_user', methods: ['GET'], format: 'json')]
     public function readOne(Request $request, UserRepository $userRepository): JsonResponse
     {
-        return $this->json(Serializer::serialize($userRepository->find($request->attributes->get('id'))));
+        try {
+            return $this->json(Serializer::serialize($userRepository->find($request->get('id'))));
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()]);
+        }
     }
 
     #[Route('/api/users/create', name: 'app_user_create', methods: ['POST'], format: 'json')]
-    public function create(Request $request, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository): JsonResponse
+    public function create(Request $request, UserRepository $userRepository): JsonResponse
     {
         try {
-            $json = json_decode($request->getContent(), true);
-            $user = new User();
-            $user->setPassword($passwordHasher->hashPassword($user, $json['password']));
-            $user->setEmail($json['email']);
-            $user->setRoles(['ROLE_USER']);
+            $user = Encoder::encode($request->getContent(), User::class);
             $userRepository->save($user);
-            return $this->json($userRepository->save($user));
+            return $this->json(Serializer::serialize($user));
         } catch (\Exception $e) {
             return $this->json(['error' => $e->getMessage()]);
         }
--- src/Encoder/Encoder.php
+++ src/Encoder/Encoder.php
@@ -1,20 +1,52 @@
 <?php
 
 namespace App\Encoder;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class Encoder
 {
-    public static function encode(string $json, $class): ?array
+
+    public function __construct(){}
+    /**
+     * @throws \ReflectionException
+     */
+    public static function encode(string $json, $class): ?object
     {
         $json = json_decode($json, true);
         $class = new \ReflectionClass($class);
         $properties = $class->getProperties();
         $entity = $class->newInstance();
         foreach ($properties as $property) {
-            $entity->{$property->getName()} = $property->getValue($entity);
+            if (array_key_exists($property->getName(), $json)) {
+                $property->setValue($entity, $json[$property->getName()]);
+            }
+            if (in_array('Symfony\Component\Security\Core\User\UserInterface', class_implements($entity))) {
+                if ($property->getName() === 'password') {
+                    $passwordHasherFactory = new PasswordHasherFactory([
+                        // auto hasher with default options for the User class (and children)
+                        User::class => ['algorithm' => 'auto'],
+
+                        // auto hasher with custom options for all PasswordAuthenticatedUserInterface instances
+                        PasswordAuthenticatedUserInterface::class => [
+                            'algorithm' => 'auto',
+                            'cost' => 15,
+                        ],
+                    ]);
+                    $passwordHasher = new UserPasswordHasher($passwordHasherFactory);
+                    $property->setValue($entity, $passwordHasher->hashPassword($entity, $property->getValue($entity)));
+                }
+                if ($property->getName() === 'roles') {
+                    $property->setValue($entity, ['ROLE_USER']);
+                }
+            }
         }
         return $entity;
     }
+
 
 
 }
